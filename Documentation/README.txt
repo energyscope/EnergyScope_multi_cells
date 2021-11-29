d25d74d2e97515ff071a666c18226cfc53489a48
Paolo Thiran <paolo.thiran@gmail.com>
2021-11-29 18:33:00 +0100

    First version of ESMCpy
    * adding a python structure to run all files from python scripts

---
+++ Documentation/README.txt
@@ -0,0 +1,7 @@
+# Documents description #
+- energy_carriers_Italy [7]: Conference paper on following case study: Whole-energy system model of Italy divided into 3 regions.
+- esmc_v1 [5]: Master thesis with the first implementation of ENergyScope Multi-Cell. The methodology is detailed here.
+
+# References #
+[5] Hernandez, A., Thiran, P., Jeanmart, H., & Limpens, G. (2020). EnergyScope Multi-Cell : A novel open-source model for multi-regional energy systems and application to a 3-cell , low-carbon energy [UCLouvain]. http://hdl.handle.net/2078.1/thesis:25229
+[7] Thiran, P., Hernandez, A., Limpens, G., Prina, M. G., Jeanmart, H., & Contino, F. (2021). Flexibility options in a multi-regional whole-energy system : the role of energy carriers in the Italian energy transition. Proceedings of ECOS 2021 - The 34th International Conference on Efficiency, Cost, Optimization, Simulation and Environmental Impact of Energy Systems, Mc, 1–12.
---
+++ esmc/__init__.py
@@ -0,0 +1,73 @@
+
+import logging.config
+import logging
+import os
+import importlib
+
+from .common import commons
+
+# Remove old log file:
+for filename in (f for f in os.listdir('.') if f.endswith('.energyscope.log')):
+    try:
+        os.remove(filename)
+    except OSError:
+        print('Could not erase previous log file ' + filename)
+
+# Logging: #
+_LOGCONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)-8s] (%(funcName)s): %(message)s',
+            'datefmt': '%y/%m/%d %H:%M:%S'
+        },
+        'notime': {
+            'format': '[%(levelname)-8s] (%(funcName)s): %(message)s',
+            'datefmt': '%y/%m/%d %H:%M:%S'
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "energyscope.misc.colorstreamhandler.ColorStreamHandler",
+            "stream": "ext://sys.stderr",
+            # "stream": "sys.stdout",
+            "level": "INFO",
+            'formatter': 'notime',
+        },
+
+        "error_file": {
+            "class": "logging.FileHandler",
+            "level": "INFO",
+            'formatter': 'standard',
+            'filename': commons['logfile'],
+            'encoding': 'utf8'
+
+        }
+    },
+
+    "root": {
+        "level": "INFO",
+        "handlers": ["console", "error_file"],
+    }
+}
+
+# Setting logging configuration:
+try:
+    logging.config.dictConfig(_LOGCONFIG)
+except Exception:
+    # if it didn't work, it might be due to ipython messing with the output
+    # typical error: Unable to configure handler 'console': IOStream has no fileno
+    # try without console output:
+    logging.warning('The colored console output is failing (possibly because of ipython). '
+                    'Switching to monochromatic output')
+    _LOGCONFIG['handlers']['console']['class'] = "logging.StreamHandler"
+    logging.config.dictConfig(_LOGCONFIG)
+
+
+from .preprocessing.preprocessing import make_dir, step1_in, step2_in, set_ampl, run_ampl, run_esmc
+from .preprocessing.TD_functions import compute_ts_from_td, compute_dc, compute_metrics
+from .postprocessing.amplpy2pd import to_pd, to_pd_pivot, get_results_pivot, get_results, get_thtd, read_output,\
+    read_T_H_TD, get_sets, get_subset, get_vars, get_params, read_json, print_json, print_case, print_step1_out
+from  .postprocessing.postprocessing import get_used, get_var_cases, subgroup, subgroup_on_max, compute_convergence
+from .postprocessing.draw_sankey.ESSankey import drawSankey
