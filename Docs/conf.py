5e8ee52cbabe4821e0ed364cbe3d39375108867e
Paolo Thiran <paolo.thiran@gmail.com>
2023-01-09 15:01:41 +0100

    :memo: Try another doc
    Previous commit did not with reathedocs.

--- Docs/conf.py
+++ Docs/conf.py
@@ -30,8 +30,12 @@ release = '2.2'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = [ 'sphinxcontrib.bibtex'
 ]
+# Bibliography:
+bibtex_bibfiles = ['refs.bib']
+bibtex_default_style = 'unsrt'
+bibtex_reference_style = 'super'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -48,9 +52,15 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+numfig = True # Add figure numbering
+numtab = True # Add table numbering
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
 
+
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+html_logo = 'images/estd_graphical_abstract.png'
