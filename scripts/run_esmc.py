13e2dbe58d866c2fcc9181d89251bf5023034ab8
Paolo Thiran <paolo.thiran@gmail.com>
2023-11-08 18:07:52 +0100

    :children_crossing: Prepare run script

--- scripts/run_esmc.py
+++ scripts/run_esmc.py
@@ -1,25 +1,25 @@
 import numpy as np
 from pathlib import Path
 
+from memory_profiler import memory_usage
+
+
 # additional line for VS studio
 import sys
 
 import pandas as pd
 
-sys.path.append('/home/asepulchre/EnergyScope_multi_cells/')
+sys.path.append('/home/pthiran/EnergyScope_multi_cells/')
 from esmc import Esmc
-from esmc.common import eu27_country_code
-
-# adding NO
-eu27_country_code.append('NO')
+from esmc.common import eu28_country_code
 
 # defining cases
-cases = ['fossil_free_ref']#, 'fossil_free_no_ned', 'fossil_free_no_h2_network',
+cases = ['fossil_free_v2']#, 'fossil_free_no_ned', 'fossil_free_no_h2_network',
          #'fossil_free_tc_max_elec_2.5GW', 'fossil_free_recycling_for_NED']
 no_imports = ['GASOLINE', 'DIESEL', 'LFO', 'GAS', 'COAL', 'H2', 'AMMONIA', 'METHANOL']
 
 # number of typical days (check that tse<0.22)
-tds = 42
+tds = 14
 
 print('Nbr_TDs', tds)
 
@@ -42,7 +42,7 @@ for c in cases:
     # define configuration
     config = {'case_study': c,
               'comment': 'none',
-              'regions_names': eu27_country_code,
+              'regions_names': eu28_country_code,
               'ref_region': 'FR',
               'gwp_limit_overall': gwp_limit_overall,
               're_share_primary': re_share_primary,
@@ -63,20 +63,6 @@ for c in cases:
     for r_code, region in my_model.regions.items():
         region.data['Resources'].loc[no_imports, 'avail_exterior'] = 0
 
-    #     # case study specific changes
-    #     if c == 'fossil_free_no_ned':
-    #         region.data['Demands'].loc['NON_ENERGY', 'INDUSTRY'] = 0
-    #     elif c == 'fossil_free_recycling_for_HVC':
-    #         region.data['Demands'].loc['NON_ENERGY', 'INDUSTRY'] *= 0.7 # we assume 30% of the feedstock can come from recycling
-    #
-    # if c == 'fossil_free_no_h2_network':
-    #     my_model.data_reg['Exch']['Network_exchnages'].loc[(slice(None), slice(None), 'H2', slice(None)), 'tc_min'] = 0
-    #     my_model.data_reg['Exch']['Network_exchnages'].loc[(slice(None), slice(None), 'H2', slice(None)), 'tc_max'] = 0
-    # elif c == 'fossil_free_tc_max_elec_2.5GW' :
-    #     my_model.data_reg['Exch']['Network_exchnages'].loc[(slice(None), slice(None), 'ELECTRICITY', slice(None)), 'tc_min'] = 0
-    #     my_model.data_reg['Exch']['Network_exchnages'].loc[(slice(None), slice(None), 'ELECTRICITY', slice(None)), 'tc_max'] = 2.5
-
-
     # Initialize and solve the temporal aggregation algorithm:
     # if already run, set algo='read' to read the solution of the clustering
     # else, set algo='kmedoid' to run kmedoid clustering algorithm to choose typical days (TDs)
@@ -85,12 +71,13 @@ for c in cases:
     else:
         my_model.init_ta(algo='read', ampl_path=ampl_path)
 
-
     # Print the time related data of the energy system optimization model using the TDs to represent it
     my_model.print_td_data()
 
     # Print data
     my_model.print_data(indep=True)
+
+
 
     # Set the Energy System Optimization Model (ESOM) as an ampl formulated problem
     my_model.set_esom(ampl_path=ampl_path)
